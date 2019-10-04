de3573adeb8e86e698c3e97a24ff0f6ea6cd62ad
victorsndvg <victorsv@gmail.com>
2019-10-04 11:24:38 +0200

    Add non-symmetric femp driver testset

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,6 +5,7 @@ using Test
 @testset "Pardiso bindings" begin include("bindings.jl") end
 @testset "Linear solver" begin include("LinearSolver.jl") end
 @testset "FEM driver" begin include("femdriver.jl") end
+@testset "Non-Symmetric FEM driver" begin include("nonsymfemdriver.jl") end
 
 end
 

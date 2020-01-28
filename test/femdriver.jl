da421e629efac0a2211b7034812afd839a5797a7
victorsndvg <victorsv@gmail.com>
2020-01-28 09:53:55 +0100

    Update femdriver.jl adn README.md.
    Remove nonsymfemdriver.jl

--- test/femdriver.jl
+++ test/femdriver.jl
@@ -1,43 +1,39 @@
 module FEMDriver
 
-# Commented waiting for new_master to support it
-# 
-# using Test
-# using Gridap
-# using GridapPardiso
-# 
-# model = CartesianDiscreteModel(
-#   domain=(0,1,0,1,0,1), partition=(10,10,10))
-# 
-# fespace = FESpace(
-#   reffe=:Lagrangian, order=1, valuetype=Float64,
-#   conformity=:H1, model=model, diritags="boundary")
-# 
-# V = TestFESpace(fespace)
-# U = TrialFESpace(fespace,0.0)
-# 
-# trian = Triangulation(model)
-# quad = CellQuadrature(trian,degree=2)
-# 
-# t_Ω = AffineFETerm(
-#   (v,u) -> inner(∇(v),∇(u)),
-#   (v) -> inner(v, (x) -> x[1]*x[2] ),
-#   trian, quad)
-# 
-# op = LinearFEOperator(V,U,t_Ω)
-# 
-# ls = PardisoSolver()
-# solver = LinearFESolver(ls)
-# 
-# uh = solve(solver,op)
-# 
-# x = free_dofs(uh)
-# A = op.mat
-# b = op.vec
-# 
-# r = A*x - b
-# 
-# tol = 1e-10
-# @test maximum(abs.(r)) < tol
+using Test
+using Gridap
+using GridapPardiso
+
+tol = 1e-10
+
+model = CartesianDiscreteModel((0,1,0,1,0,1), (10,10,10))
+
+V = TestFESpace(reffe=:Lagrangian, order=1, valuetype=Float64,
+  conformity=:H1, model=model, dirichlet_tags="boundary")
+
+U = TrialFESpace(V)
+
+trian = get_triangulation(model)
+quad = CellQuadrature(trian,2)
+
+t_Ω = AffineFETerm(
+  (v,u) -> inner(∇(v),∇(u)),
+  (v) -> inner(v, (x) -> x[1]*x[2] ),
+  trian, quad)
+
+#op = AffineFEOperator(SparseMatrixCSR{0,PetscReal,PetscInt},V,U,t_Ω)
+op = AffineFEOperator(V,U,t_Ω)
+
+ls = PardisoSolver()
+solver = LinearFESolver(ls)
+
+uh = solve(solver,op)
+
+x = get_free_values(uh)
+A = op.op.matrix
+b = op.op.vector
+
+r = A*x - b
+@test maximum(abs.(r)) < tol
 
 end #module
